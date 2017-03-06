46c864fe3a9b4d9d030dffc22acb8d9619807ebc
vpunjabi <vpunja@gmail.com>
2017-03-05 19:02:57 -0800

    Added setting options and readme file

--- Tippy/SettingsViewController.swift
+++ Tippy/SettingsViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var currenyPicker: UIPickerView!
     @IBOutlet weak var colorPreview: UIView!
     @IBOutlet weak var redColorSlider: UISlider!
     @IBOutlet weak var greenColorSlider: UISlider!
@@ -17,14 +18,25 @@ class SettingsViewController: UIViewController {
     @IBOutlet weak var defaultTipControl: UISegmentedControl!
     let defaults = UserDefaults.standard
     
+    var pickerData = NSLocale.commonISOCurrencyCodes
+    var languages = NSLocale.isoLanguageCodes
+    
     func isKeyPresentInUserDefaults(key: String) -> Bool {
-        return UserDefaults.standard.object(forKey: key) != nil
+        return defaults.object(forKey: key) != nil
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.currenyPicker.dataSource = self;
+        self.currenyPicker.delegate = self;
         let defaultTipIndex = defaults.integer(forKey: "defaultTipIndex")
         defaultTipControl.selectedSegmentIndex = defaultTipIndex
+        
+        if isKeyPresentInUserDefaults(key: "currencyCode"){
+            self.currenyPicker.selectRow(pickerData.index(of: defaults.string(forKey: "currencyCode")!)!, inComponent: 0, animated: false)
+        }else{
+            self.currenyPicker.selectRow(pickerData.index(of: NSLocale.current.currencyCode!)!, inComponent: 0, animated: false)
+        }
         
         var redColor: Float = 0
         if isKeyPresentInUserDefaults(key: "redColorSlider"){
@@ -53,6 +65,23 @@ class SettingsViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ picker: UIPickerView, numberOfRowsInComponent: Int) -> Int {
+        return pickerData.count;
+    }
+    
+    func pickerView(_ picker: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        defaults.set(NSLocale.init(localeIdentifier: pickerData[row]).displayName(forKey: .currencySymbol, value: pickerData[row]), forKey: "currencySymbol")
+        defaults.set(pickerData[row], forKey: "currencyCode")
+        defaults.set(languages[row], forKey: "languageCode")
+        defaults.synchronize()
+        return pickerData[row]
+    }
+    
     @IBAction func setDefaultTipPercentage(_ sender: AnyObject) {
         defaults.set(defaultTipControl.selectedSegmentIndex, forKey: "defaultTipIndex")
         defaults.synchronize()
--- Tippy/ViewController.swift
+++ Tippy/ViewController.swift
@@ -16,17 +16,43 @@ class ViewController: UIViewController {
     @IBOutlet weak var totalStrLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var billField: UITextField!
+    var defaultSegIndex:Int = -1
+    let defaults = UserDefaults.standard
+    var locales:[String] = NSLocale.availableLocaleIdentifiers
     override func viewDidLoad() {
         super.viewDidLoad()
         billField.becomeFirstResponder()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
+    func valueExists(kVal: String) -> Bool {
+        return defaults.object(forKey: kVal) != nil
+    }
+    
+    func getCurrencySymbol() -> String {
+        if valueExists(kVal: "currencySymbol"){
+           return defaults.string(forKey: "currencySymbol")!
+        }else{
+            return NSLocale.current.currencySymbol!
+        }
+    }
+    
+    func getLanguageCode() -> String {
+        if valueExists(kVal: "languageCode"){
+            return defaults.string(forKey: "languageCode")!
+        }else{
+            return NSLocale.current.languageCode!
+        }
+    }
+    
     override func viewWillAppear(_ animated: Bool) {
-        let defaults = UserDefaults.standard
-        let defaultSegIndex = defaults.integer(forKey: "defaultTipIndex")
-        tipControl.selectedSegmentIndex = defaultSegIndex
-        
+        billField.placeholder = getCurrencySymbol()
+        let curDefaultSegIndex = defaults.integer(forKey: "defaultTipIndex")
+        if(defaultSegIndex != curDefaultSegIndex){
+            defaultSegIndex = curDefaultSegIndex
+            tipControl.selectedSegmentIndex = defaultSegIndex
+        }
+        calculateTip(NSNull.self);
         tipControl.tintColor = UIColor(red: CGFloat(defaults.float(forKey: "redColorSlider")), green: CGFloat(defaults.float(forKey: "greenColorSlider")), blue: CGFloat(defaults.float(forKey: "blueColorSlider")), alpha: 1.0)
         
         tipStrLabel.textColor = UIColor(red: CGFloat(defaults.float(forKey: "redColorSlider")), green: CGFloat(defaults.float(forKey: "greenColorSlider")), blue: CGFloat(defaults.float(forKey: "blueColorSlider")), alpha: 1.0)
@@ -56,8 +82,27 @@ class ViewController: UIViewController {
         let tip = bill * tipPercentages[tipControl.selectedSegmentIndex];
         let total = bill + tip;
         
-        tipLabel.text = String(format: "$%.2f", arguments: [tip]);
-        totalLabel.text = String(format: "$%.2f", arguments: [total]);
+        let formatter = NumberFormatter()
+        formatter.usesGroupingSeparator = true
+
+        formatter.currencySymbol = getCurrencySymbol()
+        formatter.numberStyle = .currency
+        formatter.maximumFractionDigits = 2
+        formatter.minimumFractionDigits = 2
+        
+        formatter.locale = Locale(identifier: NSLocale.localeIdentifier(fromComponents: [NSLocale.Key.currencyCode.rawValue: defaults.string(forKey: "currencyCode")!]))
+        
+        tipLabel.text = formatter.string(from: tip as NSNumber)
+        totalLabel.text = formatter.string(from: total as NSNumber)
+        
+//        tipLabel.text = String(format: "\(getCurrencySymbol())%.2f", arguments: [tip]);
+//        totalLabel.text = String(format: "\(getCurrencySymbol())%.2f", arguments: [total]);
+        //        formatter.locale = Locale(identifier: NSLocale.localeIdentifier(fromComponents: [NSLocale.Key.currencyCode.rawValue: defaults.string(forKey: "currencyCode")!]))
+        
+        //        formatter.locale = Locale(identifier: NSLocale.init(localeIdentifier: defaults.string(forKey: "currencyCode")!).localeIdentifier)
+        
+        //        tipLabel.text = formatter.string(from: NSNumber.init(value: tip))
+        //        formatter.currencyCode = defaults.string(forKey: "currencyCode")!
     }
 }
 
